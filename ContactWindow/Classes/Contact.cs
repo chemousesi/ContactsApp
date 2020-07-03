fb477bb7bafe0ac1b3d0df07fa0a165a8e39aea0
chemousesi <ic_benaziza@esi.dz>
2020-07-03 09:07:28 +0100

    initial commit

---
+++ ContactWindow/Classes/Contact.cs
@@ -0,0 +1,24 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactWindow.Classes
+{
+    class Contact
+    {
+        [PrimaryKey, AutoIncrement]
+
+        public int Id { get; set; } 
+
+        public string Name { get; set; }
+                
+        public string Email { get; set; }
+
+        public string Phone { get; set; }
+
+    }
+}
---
+++ ContactWindow/NewContactWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using ContactWindow.Classes;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ContactWindow
+{
+    /// <summary>
+    /// Logique d'interaction pour NewContactWindow.xaml
+    /// </summary>
+    public partial class NewContactWindow : Window
+    {
+        public NewContactWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void saveButton_Click(object sender, RoutedEventArgs e)
+        {
+            //TODO: save contact 
+            Contact contact = new Contact()
+            {
+                Name = nameTextBox.Text,
+                Email = emailTextBox.Text,
+                Phone = phoneNumberTextBox.Text
+            };
+            string databaseName = "Contacts.db";
+            string folderPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            string databasePath = System.IO.Path.Combine(folderPath, databaseName);
+
+            SQLiteConnection connection = new SQLiteConnection(databasePath);
+            connection.CreateTable<Contact>();
+            connection.Insert(contact);
+            connection.Close();
+
+
+
+            this.Close();
+        }
+    }
+}
